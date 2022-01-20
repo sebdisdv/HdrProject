0e328019a6d03261952e522931132b89edaad9ce
NoeCanovi <noecanovi@gmail.com>
2022-01-20 15:31:28 +0100

    Eheh

--- main.py
+++ main.py
@@ -22,17 +22,22 @@ class HdrImplementations():
         self.settings = json.load(open("settings.json"))
         self.images_paths = self.settings["dataset"][dataset_name]
         self.images = [cv2.imread(im) for im in self.settings["dataset"][dataset_name]]
-        self.exposure_times = [get_exposure(Image.open(im)) for im in self.settings["dataset"][dataset_name]]
-        self.tonemapAlgo = cv2.createTonemapDrago()
+        #self.exposure_times = [get_exposure(Image.open(im)) for im in self.settings["dataset"][dataset_name]]
+        self.exposure_times = [0.0125, 0.125, 0.5]
+        self.tonemapAlgo = cv2.createTonemapDrago(1.0, 0.7)
         self.result_merge = None
         self.result_img = None
 
-    def applyDebevecArt(self):
+    def applyDebevecArt(self): #debevec python
         merge = cv2.createMergeDebevec()
         self.result_merge = merge.process(self.images, times= self.exposure_times.copy())
         
     def tonemap(self):
-        self.result_img = np.clip(self.tonemapAlgo.process(self.result_merge.copy()) * 255, 0, 255).astype('uint8')
+        #self.result_img = np.clip(self.tonemapAlgo.process(self.result_merge.copy()) * 255, 0, 255).astype('uint8')
+        self.result_img = self.tonemapAlgo.process(self.result_merge)
+        self.result_img = 3 * self.result_img
+        self.result_img = self.result_img * 255
+        self.result_img = np.clip(self.result_img, 0, 255).astype('uint8')
 
     def applyAceWindowed(self, image_index, window):
         self.result_merge = windowed_ace.compute(self.images_paths[image_index], window)
@@ -42,8 +47,11 @@ class HdrImplementations():
         self.result_merge = exhaustive_ace.compute(self.images_paths[image_index])
         self.tonemap()
 
-    def applyDebevec(self):
+    def applyDebevec(self): #seba python
         self.result_merge = debevec.compute(self.images, self.exposure_times)
+
+    def applyGradient(self):
+        self.result_merge = gradent.compute(self.images, self.exposure_times)
 
     def save_image(self, name):
         if self.result_img is not None:
@@ -54,8 +62,8 @@ def main():
     hdr = HdrImplementations(dataset_name="star")
     hdr.applyDebevec()
     # hdr.applyDebevec()
-    # hdr.tonemap()
-    # hdr.save_image()
+    hdr.tonemap()
+    hdr.save_image("CheGioia1.jpg")
 
 if __name__ == "__main__":
     main()
